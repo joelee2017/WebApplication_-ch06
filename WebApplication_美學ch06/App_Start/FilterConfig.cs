60e36d3f136dd336bf33ee9660bdae7e4fd81e90
joe lee <as955133@gmail.com>
2018-06-15 10:24:51 +0800

    加入專案檔案。

---
+++ WebApplication_美學ch06/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication_美學ch06
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ WebApplication_美學ch06/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication_美學ch06.MvcApplication" Language="C#" %>
