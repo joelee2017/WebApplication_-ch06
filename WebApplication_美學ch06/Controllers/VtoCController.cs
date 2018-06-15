45c4794f2370aec75cd22b74ea4497ae3759c245
joe lee <as955133@gmail.com>
2018-06-15 16:55:02 +0800

    DemoQueryString ?id= 5 參數傳值

---
+++ WebApplication_美學ch06/Controllers/VtoCController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication_美學ch06.Models;
+
+namespace WebApplication_美學ch06.Controllers
+{
+    public class VtoCController : Controller
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        // GET: VtoC
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DemoQueryString()
+        {
+            ViewBag.id = int.Parse(Request.QueryString["id"]);
+            return View();
+        }
+    }
+}
