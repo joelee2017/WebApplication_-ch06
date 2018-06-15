5b2bc8191fe54ce40bbfd0421a7e82073ac7a29f
joe lee <as955133@gmail.com>
2018-06-15 10:35:19 +0800

    TempData 跨Action 傳遞資料

---
+++ WebApplication_美學ch06/Controllers/CtoVController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication_美學ch06.Controllers
+{
+    public class CtoVController : Controller
+    {
+        // GET: CtoV
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DemoInput()
+        {
+            return View();
+        }
+
+        public ActionResult CheckInput(string name)
+        {
+            if(string.IsNullOrEmpty(name))
+            {
+                TempData["Error"] = "不得空白!";
+                return RedirectToAction("DemoInput");
+            }
+
+            ViewBag.Name = name;
+            return View();
+        }
+    }
+}
