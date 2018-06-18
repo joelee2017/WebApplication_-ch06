b474db6558af59541a89d9ca34157cca3b8936d5
joe lee <as955133@gmail.com>
2018-06-18 14:55:03 +0800

    CustomValidation
    ValidationResult  Invalid

--- WebApplication_美學ch06/Models/Partials/ProductMD.cs
+++ WebApplication_美學ch06/Models/Partials/ProductMD.cs
@@ -2,25 +2,38 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace WebApplication_美學ch06.Models
 {
+    [MetadataType(typeof(ProductMD))]
     public partial class Product
     {
-        [MetadataType(typeof(ProductMD))]
+        
         public class ProductMD
         {
             [ScaffoldColumn(false)]
             public int ProductID { get; set; }
             [Required]
             [Display(Name ="產品名稱")]
-            [StringLength(50)]
+            [CustomValidation(typeof(StringValidator),"Invalid")]
             public string ProductName { get; set; }
             [Required]
             public string QuantityPerUnit { get; set; }
             [Required]
-            public bool Discontinued { get; set; }
+            public bool Discontinued { get; set; }            
+        }
+
+        public class StringValidator
+        {
+            public static ValidationResult Invalid(String productName, ValidationContext validationContext)
+            {
+                Regex regex = new Regex(@"[^\w\.-]", RegexOptions.IgnoreCase);
+                return (productName != null && regex.Match(productName).Length > 0)
+                    ? new ValidationResult("只許含只許英數字元，句號(.)，連字號(-)。")
+                    : ValidationResult.Success;
+            }
         }
     }
 }
