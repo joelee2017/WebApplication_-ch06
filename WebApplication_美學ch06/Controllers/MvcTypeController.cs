a6eba33641b2bd44397fce9973a0b215d08380c4
joe lee <as955133@gmail.com>
2018-06-19 16:17:37 +0800

    JsonRequestBehavior Json 格式回傳

---
+++ WebApplication_美學ch06/Controllers/MvcTypeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication_美學ch06.Models;
+
+namespace WebApplication_美學ch06.Controllers
+{
+    public class MvcTypeController : Controller
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        // GET: MvcType
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DemoJson1()
+        {
+            Person person = new Person
+            {
+                Name = "Bruce",
+                Age = 18
+            };
+            return Json(person, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult DemoJson2()
+        {
+            var person = new
+            {
+                Name = "Bruce",
+                Age = 18,
+                Birthday = new DateTime(2099, 9, 9)
+            };
+            return Json(person, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult DemoJsonModel(int? id)
+        {
+            //LazyLoadingEnabled 處理循環參考 延遲加載已啟用
+            db.Configuration.LazyLoadingEnabled = false;
+            Product product = db.Products.Find(id);
+            return Json(product);
+
+        }
+    }
+}
---
+++ WebApplication_美學ch06/Controllers/OnlineGameController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication_美學ch06.Controllers
+{
+    public class OnlineGameController : Controller
+    {
+        // GET: OnlineGame
+        public ActionResult OnlineGame()
+        {
+            return View();
+        }
+
+        public ActionResult NextTime()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat($"var nextTime='{DateTime.UtcNow}';\r\n" );
+            return JavaScript(sb.ToString());
+        }
+    }
+}
