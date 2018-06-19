ea0acff01187e1b9852c0b38ff6f1489052da935
joe lee <as955133@gmail.com>
2018-06-19 08:21:59 +0800

    Customer1 客製 class驗證

---
+++ WebApplication_美學ch06/Models/EmailAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_美學ch06.Models
+{
+    public class EmailAttribute : RegularExpressionAttribute
+    {
+        // Ref: http://mvcbook.net/003m
+        public EmailAttribute() :
+            base(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+        {
+        }
+    }
+}
