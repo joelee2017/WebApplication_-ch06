eac086023adff2a113573e76a671ee00154d22ff
joe lee <as955133@gmail.com>
2018-06-22 11:59:21 +0800

    LogOutputAttribute 實作 覆寫 IActionFilter 以及 IResultFilter 介面四個方法，並在 FilterConfig 中 註冊

--- WebApplication_美學ch06/App_Start/FilterConfig.cs
+++ WebApplication_美學ch06/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using WebApplication_美學ch06.Filters;
 
 namespace WebApplication_美學ch06
 {
@@ -8,6 +9,7 @@ namespace WebApplication_美學ch06
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new LogOutputAttribute());
         }
     }
 }
---
+++ WebApplication_美學ch06/Filters/LogOutputAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication_美學ch06.Filters
+{
+    public class LogOutputAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            base.OnResultExecuting(filterContext);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            base.OnResultExecuted(filterContext);
+        }
+
+        private void log(string method, RouteData routeData)
+        {
+            var controller = routeData.Values["controller"];
+            var action = routeData.Values["action"];
+            string message = string.Format("{0} - controller : {1} action: {2}", method, controller, action);
+
+            Debug.WriteLine(message, "Action Filter Log");
+
+        }
+    }
+}
