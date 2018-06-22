c44eab2c66627e42f50b87768e2fcc11d948ae1d
joe lee <as955133@gmail.com>
2018-06-22 14:21:25 +0800

    ModelResource.en.resx 自訂語系資源檔 在去 ProductMD 中加入
    [Required(
                    ErrorMessageResourceType = typeof(ModelResource),
                    ErrorMessageResourceName = "ProductName")]
    
    並在 Web.Cofig
    
     <globalization uiCulture="auto" culture="auto"/>

--- WebApplication_美學ch06/Models/Partials/Product.cs
+++ WebApplication_美學ch06/Models/Partials/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Resources;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -16,8 +17,10 @@ namespace WebApplication_美學ch06.Models
         {
             [ScaffoldColumn(false)]
             public int ProductID { get; set; }
-            [Required]
-            [Display(Name ="產品名稱")]
+            [Required(
+                ErrorMessageResourceType = typeof(ModelResource),
+                ErrorMessageResourceName = "ProductName")]
+            [Display(Name ="產品名稱")]            
             [CustomValidation(typeof(StringValidator),"Invalid")]
             public string ProductName { get; set; }
             [Required]
