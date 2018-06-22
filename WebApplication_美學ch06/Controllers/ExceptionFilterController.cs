624588b162df97fe02faac1cc9d8ad170a134c2b
joe lee <as955133@gmail.com>
2018-06-22 11:20:20 +0800

    Exception Filter HandleError Action 上方設置

---
+++ WebApplication_美學ch06/Controllers/ExceptionFilterController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication_美學ch06.Controllers
+{
+    public class ExceptionFilterController : Controller
+    {
+        // GET: ExceptionFilter
+        [HandleError(View = "Error", ExceptionType = typeof(Exception))]
+        public ActionResult Index()
+        {
+            throw new Exception("測試 Error 頁面。");
+            //return View();
+        }
+
+        
+    }
+}
